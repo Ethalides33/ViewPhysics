15786b2b7f0935b71d00eaede991be4c51429512
Amaury <amauryba33@icloud.com>
2022-02-18 11:55:22 +0100

    Almost finished convolution

--- js/conv_functions.js
+++ js/conv_functions.js
@@ -1,11 +1,15 @@
 function Gaussian(x, mu) {
     return Math.pow(Math.E, -Math.pow(x - mu, 2) / 2) / Math.sqrt(2 * Math.PI);
 }
-function door(x) {
-    return 1;
+function door(x, x0, x1) {
+    if (x>x0 && x<x1)
+        return 1;
+    else
+        return 0;
 }
 
 var init_mu = -7;
+init_door = -8;
 const xValues = [];
 for (let x = -10; x < 10.05; x += 0.05) {
     xValues.push(x.toFixed(2));
@@ -13,51 +17,32 @@ for (let x = -10; x < 10.05; x += 0.05) {
 
 const y_door1 = [];
 xValues.forEach(x => {
-    if (x > -9 && x < -4) {
-        y_door1.push(door(x));
-    }
-    else {
-        y_door1.push(0);
-    }
+        y_door1.push(door(x, -8,-6));
 });
-console.log(y_door1.length);
+
 const y_door2 = [];
 xValues.forEach(x => {
-    if (x > 1 && x < 6) {
-        y_door2.push(door(x));
-    }
-    else {
-        y_door2.push(0)
-    }
+        y_door2.push(door(x, -1,1));
 });
-y_door2.forEach(x => console.log(x));
 
 const gen_gauss = [];
 xValues.forEach(x => {
     gen_gauss.push(Gaussian(x, 0));
-    console.log(gen_gauss[gen_gauss.length - 1]);
 });
 
 const gen_door = [];
 xValues.forEach(x => {
-    if (x > -1 && x < 1) {
-        gen_door.push(door(x));
-    }
-    else {
-        gen_door.push(0)
-    }
+    gen_door.push(door(x, -1, 1));
 });
 
 const y_gaussian1 = [];
 xValues.forEach(x => {
     y_gaussian1.push(Gaussian(x, -6));
-    console.log(y_gaussian1[y_gaussian1.length - 1]);
 });
 const y_gaussian2 = [];
 xValues.forEach(x => {
-    y_gaussian2.push(Gaussian(x, 2));
+    y_gaussian2.push(Gaussian(x, 0));
 });
-
 function convolve(array, weights) {
     if (weights.length % 2 !== 1)
       throw new Error('weights array must have an odd length');
@@ -78,3 +63,8 @@ function convolve(array, weights) {
   
     return output;
   }
+
+var functions_list = {
+    'Gaussian': gen_gauss,
+    'Door': gen_door
+}
